f33c246f8e3f9a6f1d8628b8436c29bda6332896
Davi Ribeiro <46770855+odaviribeiro@users.noreply.github.com>
2024-07-05 02:39:01 -0300

    ✨ components add task

---
+++ src/components/task/styles.ts
@@ -0,0 +1,38 @@
+import { theme } from '@ui';
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flexGrow: 1,
+    backgroundColor: theme.colors.base.gray500,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingHorizontal: 12,
+    paddingVertical: 12,
+    marginVertical: 4,
+    borderRadius: 8,
+    borderWidth: 1,
+    borderColor: theme.colors.base.gray400,
+    marginHorizontal: 24,
+    gap: 8,
+  },
+  textContainer: {
+    flexGrow: 1,
+    maxWidth: '75%',
+    height: 40,
+    alignContent: 'center',
+    justifyContent: 'center',
+    marginHorizontal: 8,
+  },
+  textDone: {
+    fontSize: theme.font_size.md,
+    color: theme.colors.base.gray300,
+    textDecorationLine: 'line-through',
+  },
+  textCreated: {
+    fontSize: theme.font_size.md,
+    color: theme.colors.base.gray100,
+    textDecorationLine: 'none',
+  },
+});
---
+++ src/components/task/task.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Pressable, Text, View } from 'react-native';
+import { styles } from './styles';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { theme } from '@ui';
+
+export type TaskProps = {
+  title: string;
+  isCompleted?: boolean;
+  onRemove: () => void;
+  onDone: () => void;
+};
+
+export function Task({ title, isCompleted, onDone, onRemove }: TaskProps) {
+  return (
+    <View style={styles.container}>
+      <Pressable onPress={onDone}>
+        <MaterialCommunityIcons
+          name={
+            isCompleted
+              ? 'checkbox-marked-circle-outline'
+              : 'checkbox-blank-circle-outline'
+          }
+          size={22}
+          color={
+            isCompleted ? theme.colors.brand.purple : theme.colors.brand.blue
+          }
+        />
+      </Pressable>
+      <View style={styles.textContainer}>
+        <Text style={isCompleted ? styles.textDone : styles.textCreated}>
+          {title}
+        </Text>
+      </View>
+      <Pressable onPress={onRemove}>
+        <MaterialCommunityIcons
+          name="trash-can-outline"
+          size={20}
+          color={theme.colors.base.gray300}
+        />
+      </Pressable>
+    </View>
+  );
+}
